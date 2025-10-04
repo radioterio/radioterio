941c6b5022e885802bce1896d3b455f6549238a3
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-04 12:14:26 +0100

    Create start and build scripts

--- packages/api-server/src/index.ts
+++ packages/api-server/src/index.ts
@@ -1,12 +1,8 @@
 import { main } from "./main.js";
 
-import makeDebug from "debug";
-
-const debug = makeDebug("app:index");
-
 try {
   await main();
 } catch (e) {
-  debug("Unhandled errors in main(): %O", e);
+  console.error("Unhandled errors in main(): %O", e);
   process.exit(1);
 }
