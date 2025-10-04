0d1a62bd8d3c14b5947b5fd7b800120590759ef7
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-04 14:18:09 +0100

    Map error kind to status code

---
+++ packages/api-server/src/errors/error-kind-to-status.ts
@@ -0,0 +1,12 @@
+import { StatusCodes } from "http-status-codes";
+import { ErrorKind } from "./kind.js";
+
+const errorKindToStatusMap = new Map<ErrorKind, StatusCodes>([
+  [ErrorKind.UserNotFound, StatusCodes.UNAUTHORIZED],
+  [ErrorKind.MissingAuthorizationHeader, StatusCodes.BAD_REQUEST],
+  [ErrorKind.IncorrectAuthorizationHeader, StatusCodes.UNAUTHORIZED],
+]);
+
+export function getStatusCode(kind: ErrorKind) {
+  return errorKindToStatusMap.get(kind);
+}
--- packages/api-server/src/router/middlewares/error.ts
+++ packages/api-server/src/router/middlewares/error.ts
@@ -2,13 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 import { AppError } from "../../errors/app-error.js";
+import { getStatusCode } from "../../errors/error-kind-to-status.js";
 
 export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
-    // TODO: Get status code for Error kind
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      error: err.kind,
-    });
+    const statusCode = getStatusCode(err.kind) ?? StatusCodes.INTERNAL_SERVER_ERROR;
+
+    res.status(statusCode).json({ error: err.kind });
     return;
   }
 
