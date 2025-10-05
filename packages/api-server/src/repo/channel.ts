892b9ca0426eb511777df26ed038446179eb3386
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-05 13:08:00 +0100

    Send only specific fields in Channel output

--- packages/api-server/src/repo/channel.ts
+++ packages/api-server/src/repo/channel.ts
@@ -1,7 +1,18 @@
 import { inject, injectable } from "inversify";
 import { KnexClient } from "../db/knex.js";
 
-const tableName = "r_streams";
+const TABLE_NAME = "r_streams";
+const ROW_FIELDS = [
+  "sid",
+  "uid",
+  "name",
+  "cover",
+  "cover_background",
+  "status",
+  "started_from",
+  "started",
+] as const;
+type RowFields = (typeof ROW_FIELDS)[number];
 
 interface StreamRow {
   readonly sid: number;
@@ -10,6 +21,8 @@ interface StreamRow {
   readonly status: number; // 0, 1, 2
   readonly started: number | null; // timestamp
   readonly started_from: number | null; // position
+  readonly cover: string | null;
+  readonly cover_background: string | null;
 }
 
 export enum ChannelStatus {
@@ -25,6 +38,8 @@ export interface Channel {
   readonly status: ChannelStatus;
   readonly startedAt: Date | null;
   readonly startedFromPosition: number | null;
+  readonly coverFile: string | null;
+  readonly coverBackgroundColor: string | null;
 }
 
 const mapStatus = (status: number) => {
@@ -40,7 +55,7 @@ const mapStatus = (status: number) => {
   }
 };
 
-const mapChannel = (row: StreamRow): Channel => ({
+const mapChannel = (row: Pick<StreamRow, RowFields>): Channel => ({
   id: row.sid,
   title: row.name,
   status: mapStatus(row.status),
@@ -48,6 +63,8 @@ const mapChannel = (row: StreamRow): Channel => ({
   startedAt: row.started !== null ? new Date(row.started) : null,
   // milliseconds offset in the playlist
   startedFromPosition: row.started_from,
+  coverFile: row.cover,
+  coverBackgroundColor: row.cover_background,
 });
 
 @injectable()
@@ -56,19 +73,19 @@ export class ChannelRepository {
 
   async getChannelsByUserId(userId: number): Promise<Channel[]> {
     const rows = await this.knex
-      .client<StreamRow>(tableName)
+      .client<StreamRow>(TABLE_NAME)
       .where("uid", userId)
-      .select("sid", "uid", "name", "status", "started_from", "started");
+      .select(...ROW_FIELDS);
 
     return rows.map(mapChannel);
   }
 
   async getChannel(channelId: number, userId: number): Promise<Channel | null> {
     const row = await this.knex
-      .client<StreamRow>(tableName)
+      .client<StreamRow>(TABLE_NAME)
       .where("sid", channelId)
       .where("uid", userId)
-      .select("sid", "uid", "name", "status", "started_from", "started")
+      .select(...ROW_FIELDS)
       .first();
 
     if (!row) {
--- packages/api-server/src/router/routes/channel/get-channels.ts
+++ packages/api-server/src/router/routes/channel/get-channels.ts
@@ -2,17 +2,52 @@ import express from "express";
 import { inject, injectable } from "inversify";
 
 import { AppAuthRequest, AuthRouteHandler } from "../../route-handler.js";
-import { Channel, ChannelRepository } from "../../../repo/channel.js";
+import { Channel, ChannelRepository, ChannelStatus } from "../../../repo/channel.js";
+import { S3Client } from "../../../fs/s3.js";
+import { getConfig } from "../../../app.js";
+
+interface ChannelOutput {
+  readonly id: number;
+  readonly title: string;
+  readonly status: ChannelStatus;
+  readonly coverFileUrl: string | null;
+  readonly coverBackgroundColor: string | null;
+}
+
+const getCoverPath = (channel: Channel) => `covers/${channel.coverFile}`;
+
+const COVER_FILE_URL_EXPIRES_IN_SECONDS = 3600; // 1 hour
 
 @injectable()
-export class GetChannelsController extends AuthRouteHandler<readonly Channel[]> {
-  constructor(@inject(ChannelRepository) private readonly channelRepository: ChannelRepository) {
+export class GetChannelsController extends AuthRouteHandler<readonly ChannelOutput[]> {
+  constructor(
+    @inject(ChannelRepository) private readonly channelRepository: ChannelRepository,
+    @inject(S3Client) private readonly s3Client: S3Client,
+  ) {
     super();
   }
 
-  async handle(req: AppAuthRequest, res: express.Response<readonly Channel[]>): Promise<void> {
-    const channels = await this.channelRepository.getChannelsByUserId(req.auth.userId);
+  async handle(
+    req: AppAuthRequest,
+    res: express.Response<readonly ChannelOutput[]>,
+  ): Promise<void> {
+    const config = getConfig(req.app);
 
-    res.json(channels);
+    const channels = await this.channelRepository.getChannelsByUserId(req.auth.userId);
+    const channelsOutput = await Promise.all(
+      channels.map(async (channel) => ({
+        id: channel.id,
+        title: channel.title,
+        status: channel.status,
+        coverFileUrl: await this.s3Client.getObjectUrl(
+          config.awsS3Bucket,
+          getCoverPath(channel),
+          COVER_FILE_URL_EXPIRES_IN_SECONDS,
+        ),
+        coverBackgroundColor: channel.coverBackgroundColor,
+      })),
+    );
+
+    res.json(channelsOutput);
   }
 }
