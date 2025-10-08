f9431f2ac3cf5b5833432d349c1c1211b6e59952
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-08 14:21:07 +0100

    Implement repeat

---
+++ packages/playback-server/src/stream-utils.ts
@@ -0,0 +1,45 @@
+import { Readable, PassThrough } from "node:stream";
+import makeDebug from "debug";
+
+const debug = makeDebug("app:stream-utils");
+
+export const repeat = (provideReadable: () => Promise<Readable>): Readable => {
+  const d = debug.extend(":repeat");
+
+  const output = new PassThrough();
+  let currentInput: Readable;
+
+  output.on("error", (err) => {
+    d("Error in output: %O", err);
+
+    if (currentInput) {
+      currentInput.destroy(err);
+      d("Input destroyed");
+    }
+  });
+
+  const handleInput = (input: Readable) => {
+    currentInput = input;
+
+    currentInput
+      .once("end", () => {
+        d("Input encoded");
+        return getNext();
+      })
+      .pipe(output, { end: false });
+  };
+
+  const handleError = (err: Error) => {
+    d("Failed to get next input: %O", err);
+    output.destroy(err);
+  };
+
+  const getNext = () => {
+    d("Get next input");
+    provideReadable().then(handleInput).catch(handleError);
+  };
+
+  getNext();
+
+  return output;
+};
