fa617bfaf0645c16c806cb66e8f5232c44a768ec
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-09 18:18:25 +0100

    Add devenv support

---
+++ packages/rtmp-encoder/devenv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+docker build -t radioterio-rtmp-encoder-dev --build-arg USER="$(id -u):$(id -g)" -f Dockerfile --target devenv .
+
+mkdir -p .cargo-cache/git
+mkdir -p .cargo-cache/registry
+mkdir -p target
+
+docker run --rm -it \
+                --network host \
+                --name radioterio-rtmp-encoder-dev \
+                -v "$(pwd)/.cargo-cache/git":/rust/.cargo/git \
+                -v "$(pwd)/.cargo-cache/registry":/rust/.cargo/registry \
+                -v "$(pwd):"/code radioterio-rtmp-encoder-dev
+                bash -c "export HOME=/rust && source /rust/.cargo/env && exec bash"
