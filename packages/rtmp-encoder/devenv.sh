6f803502fe828522497a16942f3668f55d4892e2
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-11 21:29:01 +0100

    Add optional cef

--- packages/rtmp-encoder/devenv.sh
+++ packages/rtmp-encoder/devenv.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-docker build -t radioterio-rtmp-encoder-dev --build-arg USER="$(id -u):$(id -g)" -f Dockerfile --target devenv .
+docker build -t radioterio-rtmp-encoder-dev \
+             --build-arg USER="$(id -u):$(id -g)" \
+             -f Dockerfile \
+             --target devenv-cef .
 
 mkdir -p .cargo-cache/git
 mkdir -p .cargo-cache/registry
 mkdir -p target
 
 docker run --rm -it \
-                --network host \
                 --name radioterio-rtmp-encoder-dev \
                 -v "$(pwd)/.cargo-cache/git":/rust/.cargo/git \
                 -v "$(pwd)/.cargo-cache/registry":/rust/.cargo/registry \
