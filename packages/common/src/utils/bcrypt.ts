b2740cb113678b125ed72b89a39b3fd403baea3c
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-11-23 08:25:35 +0000

    Fix Docker build: add root tsconfig.json and fix bcrypt import

--- packages/common/src/utils/bcrypt.ts
+++ packages/common/src/utils/bcrypt.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import * as bcrypt from "bcrypt";
 
 export async function verifyPassword(password: string, passwordHash: string): Promise<boolean> {
   return bcrypt.compare(password, passwordHash);
